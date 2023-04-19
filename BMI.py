37314f75c28de655a58eaa735cf4435813c2175d
unknown <sobkozan@gmail.com>
2023-04-19 22:15:51 +0200

    first commit

---
+++ BMI.py
@@ -0,0 +1,6 @@
+masa_ciala=float(input("Podaj swoja wage --> "))
+wzrost=float(input("Podaj wzrost w metrach --> "))
+
+BMI = masa_ciala/wzrost**2
+print("Twoje BMI to: ", BMI)
+print("Twoje BMI wynosi: ", round(BMI,2))
---
+++ weeks.py
@@ -0,0 +1,7 @@
+ld=7
+minuty=60
+godziny=24
+
+minuty_dzien= godziny*minuty
+minuty_tydzien=ld*minuty_dzien
+print(" Siedem tygodni to", minuty_tydzien*7,"minut")
