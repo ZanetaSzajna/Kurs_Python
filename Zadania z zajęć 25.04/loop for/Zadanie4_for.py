3015b78c323083d4627e0be47ab2ab0c2bd8a5ec
ZanetaSzajna <131194694+ZanetaSzajna@users.noreply.github.com>
2023-04-30 16:34:37 +0200

    Zadanie 4

--- Zadania z zajęć 25.04/loop for/Zadanie4_for.py
+++ Zadania z zajęć 25.04/loop for/Zadanie4_for.py
@@ -1,3 +1,12 @@
 """
 Napisz program, który wyświetli kolejne wyniki dla silni liczby naturalnej N (N podane przez użytkownika, ale nie większe niż 8).
 """
+N =int(input("Enter number <= 8 --> "))
+
+if N<= 8 and N>0:
+    silnia =1
+    for i in range(2,N+1):
+        silnia=silnia*i
+    print("Silnia z ",i,"wynosi", silnia)
+else:
+    print("I can't calculate becouse you enter number greater than 8")
