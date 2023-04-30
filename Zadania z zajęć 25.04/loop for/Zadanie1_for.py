4b41781824f36e1056549aa8fb139e5b0ed03952
ZanetaSzajna <131194694+ZanetaSzajna@users.noreply.github.com>
2023-04-30 16:44:25 +0200

    Zadanie 1

---
+++ Zadania z zajęć 25.04/loop for/Zadanie1_for.py
@@ -0,0 +1,10 @@
+"""
+Stwórz listę przedmiotów, które zabierzesz na samotną wyprawę w góry.
+ Wyświetl nazwę właśnie spakowanego przedmiotu, po ostatnim przedmiocie pokaż informację:
+ “Great, we are ready!”
+"""
+items=["Headphone", "Phone", "Books", "Tent", ]
+print("You put in ypur bag: ")
+for item in items:
+    print(item)
+print("Grat we are ready")
